b0ba6748024cecc903aa0b3c2b78bb6e53a63a13
Pravin Ghaytadak <pravinghaytadak91@gmail.com>
2026-09-24 14:36:39 +0530

    Add files via upload
    Time service

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapOpenApi();
+app.MapScalarApiReference(); 
+
+app.UseHttpsRedirection();
+
+app.MapGet("time/utc", () => Results.Ok(DateTime.UtcNow));
+
+app.Run();
